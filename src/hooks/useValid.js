058e0b22542c356b6f2c58b67898f57d961a7d83
roudranee$ git config --global user.name roudranee <ash.roudraneel@gmail.com>
2023-08-22 14:27:22 +0530

    update validation hook

--- src/hooks/useValid.js
+++ src/hooks/useValid.js
@@ -1,13 +1,16 @@
 import { useSelector } from "react-redux";
 
 const useValid = () => {
-  const { employed, companyName } = useSelector((state) => state.form);
+  const { employed, companyName, fname, lname, email } = useSelector(
+    (state) => state.form
+  );
 
-  if ((employed === "yes" && companyName.length > 0) || employed === "no") {
-    return true;
-  } else {
-    return false;
-  }
+  const isNameValid = fname.length > 0 && lname.length > 0;
+  const isEmailValid = email.length > 0;
+  const isCompanyNameValid =
+    employed === "no" || (employed === "yes" && companyName.length > 0);
+
+  return isNameValid && isEmailValid && isCompanyNameValid;
 };
 
 export default useValid;
